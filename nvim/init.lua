842fa175b241fa8ae3efdfacf0a5d79b9ff1bfdc
Thomas Letan <lthms@soap.coffee>
2026-07-17 11:02:22 +0200

    Document nvim plugin maintainer

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,8 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+-- Maintainer: Thomas Letan <lthms@soap.coffee>
+
 local M = {}
 
 M.config = {}
